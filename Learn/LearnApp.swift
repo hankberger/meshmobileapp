87e0530ccbd4676bb9a0c5a05e4805593b2b3391
hankberger <hanksberger@gmail.com>
2025-05-05 20:05:11 -0500

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by Hank Berger on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnTests/LearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTests.swift
+//  LearnTests
+//
+//  Created by Hank Berger on 5/5/25.
+//
+
+import Testing
+@testable import Learn
+
+struct LearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
